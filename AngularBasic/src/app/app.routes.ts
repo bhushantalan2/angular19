b10c0faa847385d423bc4866b5a5a103b873f843
bhushan.talan <bhushan.talan2@gmail.com>
2025-07-09 20:10:14 +0530

    angular basic component creation & add route

---
+++ AngularBasic/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { AngularBasic } from './angular-basic/angular-basic';
+
+export const routes: Routes = [
+   {path: 'AngularBasic', component: AngularBasic}
+];
